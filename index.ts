d68a6250126d6046f165125318911d930e97be1c
nrmnqdds <quddusnuriman@gmail.com>
2024-04-25 07:13:57 +0800

    :bug: update export to named export

--- index.ts
+++ index.ts
@@ -1,18 +1,3 @@
 import { Mallam } from "./services/mallam";
 
-// const mallam = new Mallam(
-//   'apikey',
-//   {
-//     top_k: 5,
-//     top_p: 1000
-//   }
-// );
-
-// const main = async () => {
-//   const res = await mallam.generatePrompt("berapa average harga rumah di jakarta?")
-//   console.log(res)
-// }
-//
-// main()
-
-module.exports = Mallam;
+module.exports = { Mallam };
